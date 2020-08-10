4ee6ea1a31f921030c27db439445b634565f3e22
Enes Ozcan <enes.ozcan@hazelcast.com>
2020-08-10 15:25:55 +0300

    Add CNAME file to creator script.

--- create.sh
+++ create.sh
@@ -11,5 +11,8 @@ antora --clean --fetch --to-dir docs antora-playbook.yml
 # Make compatible with GitHub pages
 touch docs/.nojekyll
 
+# Include custom domain
+echo "guides.hazelcast.org" > docs/CNAME
+
 echo "New site root is created at docs/index.html"
 
